ee844f635ff98d15edac8c8638c1013ae7e47bba
S H E R L O C K <97678993+SHERLOCKx90@users.noreply.github.com>
2023-06-14 02:32:47 +0530

    updated the paths

--- main/assets/dice-play.js
+++ main/assets/dice-play.js
@@ -6,7 +6,7 @@ const ChangeDice = () => {
 
   var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
 
-  var randomImageSource = "IMG/" + randomDiceImage; //images/dice1.png - images/dice6.png
+  var randomImageSource = "./" + randomDiceImage; //images/dice1.png - images/dice6.png
 
   var image1 = document.querySelectorAll("img")[0];
 
@@ -15,7 +15,7 @@ const ChangeDice = () => {
 
   var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-  var randomImageSource2 = "IMG/dice" + randomNumber2 + ".png";
+  var randomImageSource2 = "./dice" + randomNumber2 + ".png";
 
   document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
